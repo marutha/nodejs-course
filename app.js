e7c7cd72c32544a7f57bc9415247bc03124d6ab8
Maruthavanan <Maruthavanan@BNG-MC.AD.MSYSTECHNOLOGIES.COM>
2019-01-26 11:29:34 +0530

    Added debugging code

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
-const handler = require('./routes');
+const express = require('express');
 
-const server = http.createServer(handler);
+const app = express();
+
+const server = http.createServer(app);
+
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>This is add-product page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Wow this is</h1>');
+});
 
 server.listen(3000);
