3a105159b14bd473700a904d92dd2317d83cb145
maru <maruthavanan.s@gmail.com>
2019-01-27 23:00:07 +0530

    Added ejs files for all

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const handleBars = require('express-handlebars');
+// const handleBars = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', handleBars({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', handleBars({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
