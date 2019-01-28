b3320ae47af10b46fe857017261d4776279fd3ca
maru <maruthavanan.s@gmail.com>
2019-01-28 13:59:24 +0530

    Added controllers

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const handleBars = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 // app.engine('hbs', handleBars({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page Not found!!'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
